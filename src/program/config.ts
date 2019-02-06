ae8f65e0660490ddad53714ec271c013e597a107
Dizy Zeng <mhisf@vip.qq.com>
2019-02-06 16:38:48 +0800

    Add MAIL_PASSWORD config

--- src/program/config.ts
+++ src/program/config.ts
@@ -6,6 +6,9 @@ interface ProxyConfig {
   COOKIE_SECRET_KEYS: string;
   OSS_KEY: string;
   OSS_SECRET: string;
+  SMS_KEY: string;
+  SMS_SECRET: string;
+  MAIL_PASSWORD: string;
   FIREBASE_SERVER_KEY: string;
   FCM_APPLICATION_PRIVATE_KEY: string;
   FCM_APPLICATION_PUBLIC_KEY: string;
